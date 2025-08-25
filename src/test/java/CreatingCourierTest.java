9f4856a8840c6d4fa96b775d881c0708e2a96848
Klinikov Andr <azxqwe1@mail.ru>
2025-08-25 03:04:17 +0300

    добавление методов

--- src/test/java/CreatingCourierTest.java
+++ src/test/java/CreatingCourierTest.java
@@ -3,10 +3,8 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class CreatingCourierTest {
 
@@ -16,42 +14,114 @@ public class CreatingCourierTest {
     }
 
     @Test
-    public void createNewPlaceAndCheckResponse() {
-    /*    String json = "{\"login\": \"Klinikov2\", \"password\": \"10458617\", \"firstName\": \"Andrey\"}";
+    public void creatingCourierPozitive() {
+        Courier courier = new Courier("Klinikov4", "10458617", "Andrey");
         Response response =
                 given()
                         .header("Content-type", "application/json")
                         .and()
-                        .body(json)
+                        .body(courier)
                         .when()
                         .post("/api/v1/courier");
         response.then().assertThat().body("ok", equalTo(true))
                 .and()
                 .statusCode(201);
 
-
-        String json1 = "{\"login\": \"Klinikov2\", \"password\": \"10458617\"}";
-        String photoId =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json1)
-                        .when()
-                        .post("/api/v1/courier/login").then().extract().body().path("id");
-        //  response.then().assertThat().body("ok", equalTo(true))
-        //        .and()
-        //       .statusCode(200);
-*/
-        String json = "{\"id\": \"593169\"}";
-        Response response2 =
+        String json = "{\"login\": \"" + courier.getLogin() + "\", \"password\": \"" + courier.getPassword() + "\"}";
+        int id =
                 given()
                         .header("Content-type", "application/json")
                         .and()
                         .body(json)
                         .when()
-                        .delete("/api/v1/courier/:id");
-        response2.then().assertThat().body("ok", equalTo(true))
+                        .post("/api/v1/courier/login").then().extract().body().path("id");
+
+        Response responseDelete =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(json)
+                        .when()
+                        .delete("/api/v1/courier/{id}", id);
+        responseDelete.then().assertThat().body("ok", equalTo(true))
                 .and()
                 .statusCode(200);
     }
+
+    @Test
+    public void creatingCourierPovtor() {
+        Courier courier = new Courier("Klinikov7", "10458617", "Andrey");
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(courier)
+                        .when()
+                        .post("/api/v1/courier");
+        response.then().assertThat().body("ok", equalTo(true))
+                .and()
+                .statusCode(201);
+
+        Response responsePovtor =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(courier)
+                        .when()
+                        .post("/api/v1/courier");
+        responsePovtor.then().assertThat().body("message", equalTo("Этот логин уже используется. Попробуйте другой."))
+                .and()
+                .statusCode(409);
+
+        String json = "{\"login\": \"" + courier.getLogin() + "\", \"password\": \"" + courier.getPassword() + "\"}";
+        int id =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(json)
+                        .when()
+                        .post("/api/v1/courier/login").then().extract().body().path("id");
+
+        Response responseDelete =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(json)
+                        .when()
+                        .delete("/api/v1/courier/{id}", id);
+        responseDelete.then().assertThat().body("ok", equalTo(true))
+                .and()
+                .statusCode(200);
+    }
+
+    @Test
+    public void creatingCourierBezLogina() {
+        Courier courier = new Courier("", "10458617", "Andrey");
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(courier)
+                        .when()
+                        .post("/api/v1/courier");
+        response.then().assertThat().body("message", equalTo("Недостаточно данных для создания учетной записи"))
+                .and()
+                .statusCode(400);
+    }
+
+    @Test
+    public void creatingCourierBezParolya() {
+        Courier courier = new Courier("Klinikov7", "", "Andrey");
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(courier)
+                        .when()
+                        .post("/api/v1/courier");
+        response.then().assertThat().body("message", equalTo("Недостаточно данных для создания учетной записи"))
+                .and()
+                .statusCode(400);
+    }
+
 }
